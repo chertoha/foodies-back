4d492e8d4d505a43f437f05c831b2e7d88a4d834
Anton <a.chertok@akatech.com.ua>
2024-05-31 22:35:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
