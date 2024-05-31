d9bbbbdcb17dba040c122a35512f3da8a8b5ae2c
Anton <a.chertok@akatech.com.ua>
2024-05-31 22:03:21 +0100

    feat: hasky

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  {
+    plugins: { pluginJs, pluginJest },
+  },
+  {
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
+    },
+  },
+  { ignores: ["node_modules/"] },
+];
