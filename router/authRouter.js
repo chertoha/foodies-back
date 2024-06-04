216b57af5ef6b671f34cc06d20a8835112bd4b20
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-06-04 22:44:54 +0300

    feat: logging in functionality implemented

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import authControllers from "../controllers/authControllers.js";
-import { createUserSchema } from "../schemas/usersSchemas.js";
+import { createUserSchema, signinUserSchema } from "../schemas/usersSchemas.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", validateBody(createUserSchema), authControllers.registerUser);
+
+authRouter.post("/signin", validateBody(signinUserSchema), authControllers.signinUser);
 
 export default authRouter;
