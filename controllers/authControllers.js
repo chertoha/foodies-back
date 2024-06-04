6bb7471de1b7480206de2b4e87658428ebd711ce
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-06-04 22:13:02 +0300

    fix: converted services to default export

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,16 +1,16 @@
 import gravatar from "gravatar";
 
 import controllerWrapper from "../decorators/controllerWrapper.js";
-import { findUser, addUser } from "../services/usersServices.js";
+import usersServices from "../services/usersServices.js";
 import HttpError from "../helpers/HttpError.js";
 
 const registerUser = async (req, res) => {
-  const user = await findUser({ email: req.body.email });
+  const user = await usersServices.findUser({ email: req.body.email });
   if (user) {
     throw HttpError(409, "Email in use");
   }
   const avatar = gravatar.url(req.body.email, { d: "identicon" });
-  const result = await addUser({ ...req.body, avatar });
+  const result = await usersServices.addUser({ ...req.body, avatar });
   const { email, name } = result;
 
   res.status(201).json({ user: { email, name } });
--- services/usersServices.js
+++ services/usersServices.js
@@ -1,11 +1,16 @@
 import User from "../models/User.js";
 import { encryptPassword } from "../helpers/encryption.js";
 
-export async function findUser(filter) {
+async function findUser(filter) {
   return User.findOne(filter);
 }
 
-export async function addUser(body) {
+async function addUser(body) {
   const hash = await encryptPassword(body.password);
   return User.create({ ...body, password: hash });
 }
+
+export default {
+  findUser,
+  addUser,
+};
