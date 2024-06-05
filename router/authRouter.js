e4c85a032b8700c301ab275be27f467fed0c1f8a
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-06-05 08:36:54 +0300

    fix: isEmptyBody check added, hooks moved

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import authControllers from "../controllers/authControllers.js";
+import isBodyEmpty from "../middleware/isBodyEmpty.js";
 import { createUserSchema, signinUserSchema } from "../schemas/usersSchemas.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/signup", validateBody(createUserSchema), authControllers.registerUser);
+authRouter.post(
+  "/signup",
+  isBodyEmpty,
+  validateBody(createUserSchema),
+  authControllers.registerUser
+);
 
-authRouter.post("/signin", validateBody(signinUserSchema), authControllers.signinUser);
+authRouter.post("/signin", isBodyEmpty, validateBody(signinUserSchema), authControllers.signinUser);
 
 export default authRouter;
