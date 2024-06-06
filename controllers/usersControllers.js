244320b3ed4fa92ecc7943b3c8d71677f45ef84c
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-06-06 20:52:26 +0300

    avatar added

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,3 +1,6 @@
+import cloudinary from "../helpers/cloudinary.js";
+import fs from "fs/promises";
+import Jimp from "jimp";
 import controllerWrapper from "../decorators/controllerWrapper.js";
 import usersServices from "../services/usersServices.js";
 import recipesServices from "../services/recipesServices.js";
@@ -154,6 +157,32 @@ const getUserRecipes = async (req, res) => {
   res.json({ page, limit, recipesCount: userRecipes.length, ownRecipes: userRecipes });
 };
 
+const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw HttpError(400, "No file attached");
+  }
+
+  const { _id } = req.user;
+
+  const { path: filepath } = req.file;
+
+  try {
+    const image = await Jimp.read(filepath);
+    image.resize(240, 240).write(filepath);
+  } catch (error) {
+    await fs.unlink(filepath);
+    throw HttpError(400, `Could not read file. ${error.message}`);
+  }
+
+  const { secure_url } = await cloudinary.uploader.upload(filepath, { folder: "foodies" });
+
+  await fs.unlink(filepath);
+
+  const updatedUser = await usersServices.updateUserById(_id, { avatar: secure_url });
+
+  res.json({ avatar: updatedUser.avatar });
+};
+
 export default {
   getCurrentUser: controllerWrapper(getCurrentUser),
   getUser: controllerWrapper(getUser),
@@ -162,4 +191,5 @@ export default {
   removeFollowing: controllerWrapper(removeFollowing),
   getFollowers: controllerWrapper(getFollowers),
   getUserRecipes: controllerWrapper(getUserRecipes),
+  updateAvatar: controllerWrapper(updateAvatar),
 };
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,6 +2,11 @@ import express from "express";
 import usersControllers from "../controllers/usersControllers.js";
 import authenticate from "../middleware/authenticate.js";
 import isValidId from "../middleware/isValidId.js";
+import upload from "../middleware/upload.js";
+import handleMulterError from "../middleware/handleMulterError.js";
+import { allowedImageExtensions } from "../utils/imageUploadConfig.js";
+
+const configuredUpload = upload({ allowedExtensions: allowedImageExtensions }).single("avatar");
 
 const usersRouter = express.Router();
 
@@ -20,5 +25,7 @@ usersRouter.get("/:id", isValidId, usersControllers.getUser);
 usersRouter.get("/:id/followers", isValidId, usersControllers.getFollowers);
 
 usersRouter.get("/:id/recipes", isValidId, usersControllers.getUserRecipes);
+
+usersRouter.patch("/avatar", handleMulterError(configuredUpload), usersControllers.updateAvatar);
 
 export default usersRouter;
