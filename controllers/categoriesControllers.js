fe22f9c1fb9e6ebef1e21667a6250bdde8ae1238
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2024-06-10 00:36:43 +0300

    fix: categories pagination added

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -1,9 +1,18 @@
 import controllerWrapper from "../decorators/controllerWrapper.js";
-import { getAllCategories } from "../services/categoriesServices.js";
+import { countCategories, getAllCategories } from "../services/categoriesServices.js";
 
 const getCategories = async (req, res) => {
-  const categories = await getAllCategories();
-  res.json(categories);
+  let { page = 1, limit = 12 } = req.query;
+  page = Math.floor(Number(page));
+  limit = Math.floor(Number(limit));
+  if (isNaN(page) || isNaN(limit)) {
+    throw HttpError(400, "Page and limit must be integers");
+  }
+  const skip = (page - 1) * limit;
+  const settings = { skip, limit };
+  const categories = await getAllCategories({ settings });
+  const categoriesCount = await countCategories();
+  res.json({ page, limit, categoriesCount, categories });
 };
 
 export default {
