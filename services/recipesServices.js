4f695b30fcbc84b08200bf89c3602269bd533dc8
Andrii Popov <Popov.Andrey80@gmail.com>
2024-06-10 19:54:18 +0300

    add populate to recipe service

--- services/recipesServices.js
+++ services/recipesServices.js
@@ -3,12 +3,12 @@ import Recipe from "../models/Recipe.js";
 const getRecipeList = (search = {}) => {
     const { filter, fields, settings } = search;
     // console.log("getRecipeList search >>>", search)
-    return Recipe.find(filter, fields, settings)
+    return Recipe.find(filter, fields, settings).populate("owner", "name avatar")
 };
 
 const countRecipes = filter => Recipe.countDocuments(filter);
 
-const getRecipe = filter => Recipe.findOne(filter);
+const getRecipe = filter => Recipe.findOne(filter).populate("owner", "name avatar");
 
 const addRecipe = data => Recipe.create(data);
 
